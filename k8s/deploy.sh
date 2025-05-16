fcec453defa12d2bf4388521c16975dd3353acce
rihamesabik <rihame2005.28@gmail.com>
2025-05-16 10:28:51 +0200

    deploy.sh2

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo "🔄 Déploiement en cours sur le cluster K3s local..."
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f .
 echo "✅ Déploiement terminé !"
